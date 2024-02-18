d93e742c76ce76487e0337af2d46f77d0464079c
MS_D_rei <msmdmission1111@gmail.com>
2024-02-18 22:39:44 +0900

    Create: easy/unshift.ts

---
+++ easy/unshift.ts
@@ -0,0 +1,28 @@
+/*
+  3060 - Unshift
+
+  ### Question
+
+  Implement the type version of ```Array.unshift```
+
+  For example:
+
+  ```typescript
+  type Result = Unshift<[1, 2], 0> // [0, 1, 2,]
+  ```
+
+  > View on GitHub: https://tsch.js.org/3060
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Unshift<T extends any[], U> = [U, ...T];
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "../utils";
+
+type cases = [
+  Expect<Equal<Unshift<[], 1>, [1]>>,
+  Expect<Equal<Unshift<[1, 2], 0>, [0, 1, 2]>>,
+  Expect<Equal<Unshift<["1", 2, "3"], boolean>, [boolean, "1", 2, "3"]>>,
+];
