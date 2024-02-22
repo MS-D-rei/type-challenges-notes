77cdd5ae6741c2012d1223818f4fa3ef87a7caf7
MS_D_rei <msmdmission1111@gmail.com>
2024-02-22 16:12:44 +0900

    Add: original PR content of variadic tuple type
    in typescript-handbook/variadic-tuple-type.ts

--- typescript-handbook/variadic-tuple-type.ts
+++ typescript-handbook/variadic-tuple-type.ts
@@ -44,3 +44,128 @@ const foo1 = (x: string, y: number, z: boolean) => {};
 // const partialFoo2 = partialCall(foo1, "hello", 100, true, "world"); // expected 4 arguments, but got 5
 const partialFoo3 = partialCall(foo1, "hello");
 const callPartialFoo3 = partialFoo3(100, true);
+
+
+// https://github.com/microsoft/TypeScript/pull/39094
+
+// Type relationships
+/*
+Generally, a tuple type S is related to a tuple type T by pairwise relating elements of S to the elements of T. Variadic elements are processed as follows:
+- A variadic element ...U in S is related to a variadic element ...V in T if U is related to V.
+- A variadic element ...U in S is related to a rest element ...X[] in T if U is related to X[].
+*/
+function relationship1<T extends unknown[], U extends T>(x: [string, ...unknown[]], y: [string, ...T], z: [string, ...U]) {
+  x = y;
+  x = z;
+  // y = x; // Error
+  y = z; // assignable because U extends T
+  // z = x; // Error
+  // z = y; // Error
+}
+/*
+Tuple types with single variadic elements have the following relations:
+- [...T] is related to T.
+- T is related to readonly [...T].
+- T is related to [...T] when T is constrained to a mutable array or tuple type.
+*/
+function relationship2<T extends readonly unknown[]>(t: T, m: [...T], r: readonly [...T]) {
+  t = m;
+  // t = r; // Error
+  // m = t; // Error readonly cannot be assigned to mutable
+  // m = r; // Error readonly cannot be assigned to mutable
+  r = t;
+  r = m; // mutable can be assigned to readonly
+}
+
+// Type inference
+type First<T extends readonly unknown[]> = T[0];
+type DropFirst<T extends readonly unknown[]> = T extends readonly [unknown?, ...infer U] ? U : [...T];  
+type Last<T extends readonly unknown[]> =
+  T extends readonly [...infer _, infer U] ? U :
+  T extends readonly [...infer _, (infer U)?] ? U | undefined :
+  undefined;
+type DropLast <T extends readonly unknown[]> = T extends readonly [...infer U, unknown?] ? U : [...T];
+
+type InferenceFirst = First<[number, string, boolean]>; // number
+type InferenceDropFirst = DropFirst<[number, string, boolean]>; // [string, boolean]
+type InferenceLast = Last<[number, string, boolean]>; // boolean
+type InferenceDropLast = DropLast<[number, string, boolean]>; // [number, string]
+
+// Spread in array literals
+// when a spread occurs in an array literal,
+// it is processed as a variadic element in the resulting tuple type
+// [10, true] will be [number, boolean],
+// so [1, ...T, 2, ...U, 3] as const will be [1, string, 2, number, boolean, 3] as const
+function spreadArray<T extends unknown[], U extends unknown[]>(t: [...T], u: [...U]) {
+  return [1, ...t, 2, ...u, 3] as const;
+}
+const spreadArrayResult = spreadArray(["hello"], [10, true]); 
+// expected to be readonly [1, string, 2, number, boolean, 3]
+// not [1, "hello", 2, 10, true, 3]
+
+declare function spreadArray1<T extends unknown[]>(t: T): T;
+declare function spreadArray2<T extends unknown[]>(t: T): readonly [...T];
+declare function spreadArray3<T extends unknown[]>(t: [...T]): T;
+declare function spreadArray4<T extends unknown[]>(t: [...T]): readonly [...T];
+
+const spreadArray1Result = spreadArray1(["hello", 42]); // expected to be (string | number)[]
+const spreadArray2Result = spreadArray2(["hello", 42]); // expected to be readonly (string | number)[]
+const spreadArray3Result = spreadArray3(["hello", 42]); // expected to be [string, number]
+const spreadArray4Result = spreadArray4(["hello", 42]); // expected to be readonly [string, number]
+
+const testArray10 = ["hello", 42];
+type TestArray10 = typeof testArray10; // expected to be (string | number)[]
+/**
+ * Represents a tuple type that spreads the elements of `TestArray10`.
+ * The expected type is [string, number].
+ */
+type SpreadArray10 = [...TestArray10]; // expected to be [string, number]
+type SpreadArray10Boolean = [...TestArray10, boolean]; // expected to be [string, number, boolean]
+
+// Indexing
+function indexing1<T extends unknown[]>(t: [string, ...T], n: number) {
+  const a = t[0]; // string
+  const b = t[1]; // [string, ...T][1]
+  const c = t[2]; // [string, ...T][2]
+  const d = t[n]; // [string, ...T][number]
+};
+
+function indexing2<T extends unknown[]>(t: [string, ...T, number], n: number) {
+  const a = t[0]; // string
+  const b = t[1]; // [string, ...T, number][1]
+  const c = t[2]; // [string, ...T, number][2]
+  const d = t[n]; // [string, ...T, number][number]
+}
+
+// Destructuring
+function destructuring1<T extends unknown[]>(t: [string, ...T]) {
+  const [...an] = t; // [string, ...T]
+  const [b1, ...bn] = t; // string, [...T]
+  const [c1, c2, ...cn] = t; // string, [string, ...T][1], T[number][]
+} 
+
+function destructuring2<T extends unknown[]>(t: [string, ...T, number]) {
+  const [...an] = t; // [string, ...T, number]
+  const [b1, ...bn] = t; // string, [...T, number]
+  const [c1, c2, ...cn] = t; // string, [...T, number][1], (number | T[number])[]
+}
+
+// Rest parameters and Spread arguments
+
+declare function spreadArgs1(a: number, b: string, c: boolean, ...d: number[]): void;
+
+function spreadArgs2(t1: [number, string], t2: [boolean], a1: number[]) {
+  spreadArgs1(1, 'abc', true, 42, 43, 44);
+  spreadArgs1(...t1, true, ...a1);
+  spreadArgs1(...t1, ...t2, ...a1);
+  // spreadArgs1(...t1); // Error: expected 3 arguments, but got 2
+  // spreadArgs1(...t1, 45); // Error: number is not assignable to boolean
+}
+
+declare function restParams1<T extends unknown[]>(x: number, ...args: [...T, number]): T;
+
+function restParams2<U extends unknown[]>(u: U) {
+  restParams1(1, 2); // expected to be []
+  restParams1(1, "hello", true, 2); // expected to be [string, boolean]
+  restParams1(1, ...u, "hi", 2); // expected to be [...U, string]
+}
