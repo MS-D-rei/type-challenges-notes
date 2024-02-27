5c6c6a6ef15bb9442fe644449c6099caefe294d7
MS_D_rei <msmdmission1111@gmail.com>
2024-02-28 00:27:50 +0900

    Add: more explanation to medium/length-of-string.ts

--- medium/length-of-string.ts
+++ medium/length-of-string.ts
@@ -26,12 +26,23 @@ type cases = [
 
 /* _____________ Your Code Here _____________ */
 
-type LengthOfString<S extends string> = S extends ''
-  ? 0
-  : ConvertStringToArray<S>['length']
+type LengthOfString<S extends string> = ConvertStringToArray<S>['length']
 
 type ConvertStringToArray<S extends string> = S extends `${infer F}${infer R}`
-  ? [F, ...ConvertStringToArray<R>]
+  ? [F, ...ConvertStringToArray<R>] // tuple type
   : []
 
 type TestConvertStringToArray = ConvertStringToArray<'abcd'>
+type LengthOfTestConvertStringToArray = TestConvertStringToArray['length'] // 4
+
+type TestLengthOfString = LengthOfString<'abcd'>
+
+/* _____________ Points _____________ */
+// - How to get concrete number from string?
+// => tuple type can be used to get the length of string.
+const abcdString = 'abcd' // string type
+type NumberOfAbcdString = (typeof abcdString)['length'] // number, not 4.
+const abcdArray = ['a', 'b', 'c', 'd'] // array type
+type NumberOfAbcdArray = (typeof abcdArray)['length'] // number, not 4.
+const abcdArrayAsConst = ['a', 'b', 'c', 'd'] as const // tuple type
+type NumberOfAbcdArrayAsConst = (typeof abcdArrayAsConst)['length'] // 4
